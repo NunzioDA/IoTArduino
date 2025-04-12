916fe569c4d4a49fb59dfffc84d0c50b984937fc
NunzioDA <nunziodamore99@gmail.com>
2025-04-12 09:03:10 +0200

    rgb_dimmable_light first commit

---
+++ rgb_dimmable_light.cpp
@@ -0,0 +1,51 @@
+#include "rgb_dimmable_light.h"
+#include "Arduino.h"
+
+RGBDimmableLight::RGBDimmableLight(int r_pin, int g_pin, int b_pin) : r_pin(r_pin), g_pin(g_pin), b_pin(b_pin), r_intensity(255), g_intensity(255), b_intensity(255) {
+  pinMode(this-> r_pin, OUTPUT);
+  pinMode(this-> g_pin, OUTPUT);
+  pinMode(this-> b_pin, OUTPUT);
+}
+
+void RGBDimmableLight::setColor(String hexColor)
+{
+  if (hexColor[0] == '#') {
+    hexColor = hexColor.substring(1);
+  }
+
+  long number = strtol(hexColor.c_str(), NULL, 16);
+
+  byte red   = (number >> 16) & 0xFF;
+  byte green = (number >> 8) & 0xFF;
+  byte blue  = number & 0xFF;
+
+  Serial.println(red);
+  Serial.println(green);
+  Serial.println(blue);
+
+  int red_intensity = (int)red;
+  int green_intensity = (int)green;
+  int blue_intensity = (int)blue;
+
+  this -> r_intensity = red_intensity;
+  this -> g_intensity = green_intensity;
+  this -> b_intensity = blue_intensity;
+
+  analogWrite(this -> r_pin, red_intensity);
+  analogWrite(this -> g_pin, green_intensity);
+  analogWrite(this -> b_pin, blue_intensity);
+}
+
+void RGBDimmableLight::on()
+{
+  analogWrite(this -> r_pin, this -> r_intensity);
+  analogWrite(this -> g_pin, this -> g_intensity);
+  analogWrite(this -> b_pin, this -> b_intensity);
+}
+
+void RGBDimmableLight::off()
+{
+  analogWrite(this -> r_pin, 0);
+  analogWrite(this -> g_pin, 0);
+  analogWrite(this -> b_pin, 0);
+}
---
+++ rgb_dimmable_light.h
@@ -0,0 +1,70 @@
+#ifndef RGBDimmableLight_h
+#define RGBDimmableLight_h
+
+#include <Arduino.h>
+
+/**
+  * Author: Nunzio D'Amore.
+  * Date: 09/04/2025
+  *
+  * @brief RGBDimmableLight is a class for controlling an RGB LED where each color channel can be dimmed.
+  *
+  * This class allows setting the color of an RGB LED by specifying a color name.
+  * It assumes that the connected pins support PWM (Pulse Width Modulation) for dimming
+  * the intensity of each red, green, and blue component.
+  *
+  * ## Usage:
+  * ```cpp
+  * RGBDimmableLight myLight(3, 5, 6); // Define an RGB LED connected to digital pins 3 (R), 5 (G), 6 (B)
+  * myLight.setColor("#F52E9D");      // Set the LED to red
+  * // ... other color names can be supported in the implementation
+  * ```
+ */
+class RGBDimmableLight{
+ public:
+  /**
+   * @brief Constructor for the RGBDimmableLight class.
+   *
+   * @param r_pin The pin connected to the red LED component.
+   * @param g_pin The pin connected to the green LED component.
+   * @param b_pin The pin connected to the blue LED component.
+   * It is expected that these pins support PWM output for dimming functionality
+  */
+  RGBDimmableLight(int r_pin, int g_pin, int b_pin);
+
+  /**
+   * @brief Sets the color of the RGB LED based on a color name.
+   *
+   * This method takes a string representing the desired color and sets the
+   * appropriate intensity levels for the red, green, and blue LED components.
+   *
+   * @param hexColor A string representing the desired color in hexadecimal.
+  */
+  void setColor(String hexColor);
+
+  /**
+   * @brief Switches the light on
+   *
+   * This method is used to switch the light on
+   *
+  */
+  void on();
+
+  /**
+   * @brief Switches the light off
+   *
+   * This method is used to switch the light off
+   *
+  */
+  void off();
+
+ private:
+  int r_pin; // pin connected to the red LED component.
+  int g_pin; // pin connected to the green LED component.
+  int b_pin; // pin connected to the blue LED component.
+  int r_intensity; // intensity of the red LED component.
+  int g_intensity; // intensity of the blue LED component.
+  int b_intensity; // intensity of the green LED component.
+};
+
+#endif
