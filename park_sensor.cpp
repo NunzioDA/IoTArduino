1c4c6a6dc9c4abf0b7868a5e3cc604cbfeec9a57
NunzioDA <nunziodamore99@gmail.com>
2025-05-04 00:52:36 +0200

    using directly proximity sensor distance

--- park_sensor.cpp
+++ park_sensor.cpp
@@ -2,16 +2,18 @@
 #include "Arduino.h"
 #include "proximity_sensor.h"
 
-ParkSensor::ParkSensor(int trigger, int echo) : proximitySensor(trigger, echo), distance_noise_reduction(0){}
+ParkSensor::ParkSensor(int trigger, int echo) : proximitySensor(trigger, echo), distance_noise_reduction(10){}
 
 bool ParkSensor::isParked()
 {
-  return this -> distance() < 5;
+  bool returnValue = this -> distance() < 9;
+  return returnValue;
 }
 
 double ParkSensor::distance()
 {
-  this -> distance_noise_reduction += this ->  proximitySensor.distance();
-  this -> distance_noise_reduction = this -> distance_noise_reduction / 3;
-  return this -> distance_noise_reduction;
+  //this -> distance_noise_reduction = (1.5*this -> distance_noise_reduction + this ->  proximitySensor.distance())/ 2.5;
+
+  delay(5); 
+  return this ->  proximitySensor.distance();
 }
